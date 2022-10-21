789c2af1f1ff13bfc5154ab87a9c4b7c5c2d11d7
Douglas Aguiar <daguiaralcantara@gmail.com>
2022-10-21 13:10:43 -0300

    Implementação da função filter_by_salary_range.

--- src/insights.py
+++ src/insights.py
@@ -63,44 +63,42 @@ def get_min_salary(path):
 
 
 def matches_salary_range(job, salary):
-    """Checks if a given salary is in the salary range of a given job
+    if (
+        "min_salary" not in job
+        or "max_salary" not in job
+    ):
+        raise ValueError()
 
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
+    min_salary = job["min_salary"]
+    max_salary = job["max_salary"]
 
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
+    if (
+        not isinstance(min_salary, int)
+        or not isinstance(max_salary, int)
+        or int(min_salary) > int(max_salary)
+        or not isinstance(salary, int)
+    ):
+        raise ValueError()
 
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    pass
+    if int(min_salary) <= salary <= int(max_salary):
+        return True
+    else:
+        return False
+
+
+def try_valid_jobs(job, salary):
+    try:
+        if matches_salary_range(job, salary):
+            return job
+    except ValueError:
+        return None
 
 
 def filter_by_salary_range(jobs, salary):
-    """Filters a list of jobs by salary range
+    valid_salaries = []
+    for job in jobs:
+        valid = try_valid_jobs(job, salary)
+        if valid is not None:
+            valid_salaries.append(job)
 
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    return []
+    return valid_salaries
