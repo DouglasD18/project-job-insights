15acdf67443b86c666e3695e4202fad938cc0a3b
Douglas Aguiar <daguiaralcantara@gmail.com>
2022-10-22 12:46:07 -0300

    Implementação de testes para o método count_ocurrences do módulo counter.

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,17 @@
 # from src.counter import count_ocurrences
+from src.counter import count_ocurrences
+import pytest
+
+path = 'src/jobs.csv'
 
 
 def test_counter():
-    pass
+    "Testa retorno para a palavra Batista"
+    assert count_ocurrences(path, "Batista") == 30
+
+    "Testa retorno para a palavra baTista"
+    assert count_ocurrences(path, "baTista") == 30
+
+    "Testa resultado passando um número"
+    with pytest.raises(AttributeError):
+        count_ocurrences(path, 18)
