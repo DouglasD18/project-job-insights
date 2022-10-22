15b231d02f377200bae0dca9ba71a7d7f45572b8
Douglas Aguiar <daguiaralcantara@gmail.com>
2022-10-22 13:21:03 -0300

    Implementação do teste da do método read_brazilian_jobs do módulo brazilians_jobs.

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,13 @@
 # from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
+
+path = 'tests/mocks/brazilians_jobs.csv'
 
 
 def test_brazilian_jobs():
-    pass
+    "Testa o retorno dos headers"
+    dic = read_brazilian_file(path)[0]
+    keys = list(dic)
+    assert keys[0] == 'title'
+    assert keys[1] == 'salary'
+    assert keys[2] == 'type'
