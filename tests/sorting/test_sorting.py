9175e08bf37ad15871d0868258aa27550b8aece6
Douglas Aguiar <daguiaralcantara@gmail.com>
2022-10-23 13:36:19 -0300

    Finalizando a implementação dos requisitos obrigatórios.

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,22 @@
 # from src.sorting import sort_by
+from src.sorting import sort_by
+import pytest
+
+
+def list_dicts():
+    return [
+        {"date_posted": "2021-11-09", "min_salary": 3000, "max_salary": 7740},
+        {"date_posted": "2022-08-18", "min_salary": 3500, "max_salary": 9750},
+        {"date_posted": "2020-10-18", "min_salary": 5000, "max_salary": 8000}
+    ]
 
 
 def test_sort_by_criteria():
-    pass
+    jobs = list_dicts()
+    "Testa se funciona corretamente"
+    sort_by(jobs, "max_salary")
+    assert jobs[1]["max_salary"] == 8000
+
+    "Testa resultado passando um critério errado"
+    with pytest.raises(ValueError):
+        sort_by(jobs, "salary")
